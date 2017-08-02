e29a29e929cecf2731e6d1ce25f82a6f217c8728
Navjot Singh <weavebytes@gmail.com>
2017-08-02 18:42:38 +0530

    code to write value in settings file

--- demo.py
+++ demo.py
@@ -6,3 +6,11 @@ print "Sections: ", parser.sections()
 
 print "Network:", parser.options('Network')
 print "Network->Host:", parser.get('Network', 'Host')
+
+parser.set('Network', 'Port', '40')
+print "Network->Port:", parser.get('Network', 'Port')
+
+# Writing our configuration file to 'settings.ini'
+#with open('settings.ini', 'wb') as configfile:
+with open('settings.ini', 'w') as configfile:
+    parser.write(configfile)
